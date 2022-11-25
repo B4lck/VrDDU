48d03452d31347c3dc969700b455f2b4213817dd
B4lck <nikolaisaldernbalck@gmail.com>
2022-11-25 12:01:38 +0100

    idk

--- Assets/Tallerken.cs
+++ Assets/Tallerken.cs
@@ -19,6 +19,7 @@ public class tallerken : MonoBehaviour
         for (int i = 0; i < ingredienser.Count; i++)
         {
             Transform obj = ingredienser[i].transform;
+            Debug.Log(obj.name);
             obj.position = transform.position + (Vector3.up * i * 5);
             obj.rotation = transform.rotation;
         }
@@ -26,6 +27,7 @@ public class tallerken : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        Debug.Log(collision.gameObject.name);
         if (collision.gameObject.GetComponent<Ingredient>())
         {
             add(collision.gameObject.GetComponent<Ingredient>());
