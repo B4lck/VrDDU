cb51259f1be591983e0d91d0a1abf9014866601a
B4lck <nikolaisaldernbalck@gmail.com>
2023-01-06 13:05:53 +0100

    Revert "TIng"
    This reverts commit db84f4170848b09f6a83e8b85d25ac2b8b611715.

--- Assets/UiOrdrerManager.cs
+++ Assets/UiOrdrerManager.cs
@@ -1,37 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class UiOrdrerManager : MonoBehaviour
 {
-    public BurgerOrderManager orderMan;
-    [SerializeField]
-    public Sprite[] sprites;
-    /*
-     0. Bund 
-     1. Bacon
-     2. Bøf
-     3. Ketchup
-     4. Tomat
-     5. Top
-     */
+    public OrderManager order;
+
 
     // Update is called once per frame
     void Update()
     {
-        foreach (Order ordre in orderMan.Orders)
-        {
-            if (!ordre) continue;
-            foreach (Ingredient.IngredientType ingredient in ordre.ingredienser)
-            {
-                int i = System.Array.IndexOf(ordre.ingredienser.ToArray(), ingredient);
-                switch (ingredient)
-                {
-                    default:
-                        break;
-                }
-            }
-        }
+        
     }
 }
